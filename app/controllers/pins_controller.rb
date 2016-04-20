dbe4bc1a9170e4a9942ebd1f55a2e8f63b04f30c
siwekmichal <siwekmichal@gmail.com>
2016-04-20 12:34:30 +0000

    powiązanie pinów z użytkownikami i uwierzytelnienie

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,7 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show] 
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
   # GET /pins
   # GET /pins.json
@@ -14,7 +16,8 @@ class PinsController < ApplicationController
 
   # GET /pins/new
   def new
-    @pin = Pin.new
+    # @pin = Pin.new
+    @pin = current_user.pins.build
   end
 
   # GET /pins/1/edit
@@ -24,8 +27,9 @@ class PinsController < ApplicationController
   # POST /pins
   # POST /pins.json
   def create
-    @pin = Pin.new(pin_params)
-
+    # @pin = Pin.new(pin_params)
+    @pin = current_user.pins.build(pin_params)
+    
     respond_to do |format|
       if @pin.save
         format.html { redirect_to @pin, notice: 'Pin utworzony pomyślnie.' }
@@ -70,5 +74,10 @@ class PinsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
       params.require(:pin).permit(:description)
+    end
+    
+    def correct_user
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "Nie jesteś uprawniony do edycji tego pinu" if @pin.nil?
     end
 end
